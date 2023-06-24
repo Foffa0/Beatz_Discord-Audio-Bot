43bcbf660192038cf6a58dccabcd141e860488da
Foffa <schmuck.christopher@gmail.com>
2023-06-24 23:14:04 +0200

    add youtube channel attributes

--- song.py
+++ song.py
@@ -1,10 +1,12 @@
 
 class Song():
 
-    def __init__(self, origin, host, url, title=None, duration=None, thumbnail=None):
+    def __init__(self, origin, host, url, title=None, duration=None, thumbnail=None, channel=None, channel_thumbnail=None):
         self.origin = origin
         self.host = host
         self.title = title
         self.url = url
         self.duration = duration
         self.thumbnail = thumbnail
+        self.channel = channel
+        self.channel_thumbnail = channel_thumbnail
